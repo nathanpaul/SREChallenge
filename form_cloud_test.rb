25783c6abfa6ecca5ecab0da01319defc6de2231
Paul Nathan <nathanp1@tcnj.edu>
2017-09-19 13:44:49 -0400

    Uploaded file to bucket, updated YAML file for cloudFront stack

--- form_cloud_test.rb
+++ form_cloud_test.rb
@@ -1,6 +1,6 @@
 require 'aws-sdk'
 require 'yaml'
-require 'json'
+require 'pry'
 
 keys = YAML.load_file('secret_keys.yaml')
 ENV['AWS_ACCESS_KEY_ID'] = keys['aws']['access_key']
@@ -10,10 +10,33 @@ ENV['AWS_REGION'] = 'us-east-1'
 file = File.read('cloudformation.yaml')
 
 cf = Aws::CloudFormation::Client.new(validate_params: false)
-stack = cf.create_stack({
-    stack_name: "my-stack",
-    template_body: file,
-    parameters: [
-    { parameter_key: 'CertificateARN', parameter_value: keys['aws']['certificate_arn'] },
-    { parameter_key: 'RootDomainName', parameter_value: keys['aws']['domain_name']} ]
+domain_name = keys['aws']['domain_name']
+begin
+  puts "creating stack"
+  stack = cf.create_stack({
+      stack_name: "my-stack",
+      template_body: file,
+      parameters: [
+      { parameter_key: 'CertificateARN', parameter_value: keys['aws']['certificate_arn'] },
+      { parameter_key: 'RootDomainName', parameter_value: domain_name} ]
+  })
+  #WAIT
+rescue
+  puts "created stack already"
+end
+
+# once done, upload to root bucket with the index.html file.
+puts "uploading file"
+index_file_name = "index.html"
+index_file = File.read("index.html")
+client = Aws::S3::Client.new
+client.put_bucket_acl({
+  acl: "public-read",
+  bucket: domain_name,
 })
+client.put_object({
+  bucket: domain_name,
+  key: index_file_name,
+  body: index_file
+})
+# test that it's working with Net URI
