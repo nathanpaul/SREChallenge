15af80465af74a113bcfc7e539aa775562c603a6
Paul Nathan <nathanp1@tcnj.edu>
2017-09-19 12:45:25 -0400

    Creation through ruby script done

---
+++ form_cloud_test.rb
@@ -0,0 +1,20 @@
+require 'aws-sdk'
+require 'yaml'
+require 'json'
+
+keys = YAML.load_file('secret_keys.yaml')
+ENV['AWS_ACCESS_KEY_ID'] = keys['aws']['access_key']
+ENV['AWS_SECRET_ACCESS_KEY'] = keys['aws']['secret_key']
+ENV['AWS_REGION'] = 'us-east-1'
+
+file = File.read('cloudformation.yaml')
+
+cf = Aws::CloudFormation::Client.new(validate_params: false)
+{CertificateARN: keys['aws']['certificate_arn'], RootDomainName: keys['aws']['domain_name']}
+stack = cf.create_stack({
+    stack_name: "my-stack",
+    template_body: file,
+    parameters: [
+    { parameter_key: 'CertificateARN', parameter_value: keys['aws']['certificate_arn'] },
+    { parameter_key: 'RootDomainName', parameter_value: keys['aws']['domain_name']} ]
+})
